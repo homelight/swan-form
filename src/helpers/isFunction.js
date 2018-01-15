9ac8b41fad0f4fc75c8d76ea0d7d87ae86af2235
Shawn Patrick Rice <rice@shawnrice.org>
2018-01-15 00:52:47 -0500

    add todo

--- src/helpers/isFunction.js
+++ src/helpers/isFunction.js
@@ -1,3 +1,4 @@
+// @TODO consider replacing with the lodash function
 export default function isFunction(func) {
   return typeof func === 'function';
 }
--- src/helpers/isObject.js
+++ src/helpers/isObject.js
@@ -1,3 +1,4 @@
+// @TODO consider replacing with the lodash function
 export default function isObject(obj) {
   return obj !== undefined && obj !== null && Object.prototype.constructor === obj.constructor;
 }
