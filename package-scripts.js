7207b7f059113fefd44ba9008065408949bdbadb
Shawn Patrick Rice <rice@shawnrice.org>
2018-01-23 15:35:30 -0500

    add in nps scripts for repo-level convenience

---
+++ package-scripts.js
@@ -0,0 +1,26 @@
+const npsUtils = require('nps-utils');
+
+const getDevCmd = project =>
+  `babel --src-type module "packages/${project}/src" --out-dir "packages/${project}/dist" --ignore "packages/${project}/src/*.test.js" --watch`;
+
+module.exports = {
+  // @todo expand these scripts
+  scripts: {
+    dev: {
+      all: npsUtils.concurrent.nps(
+        'dev.helpers',
+        'dev.form',
+        'dev.field',
+        'dev.extra-fields',
+        'dev.slider',
+        'dev.example',
+      ),
+      helpers: getDevCmd('helpers'),
+      form: getDevCmd('form'),
+      field: getDevCmd('field'),
+      'extra-fields': getDevCmd('extra-fields'),
+      slider: getDevCmd('slider'),
+      example: 'cd packages/example && yarn start',
+    },
+  },
+};
