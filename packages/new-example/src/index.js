8bf4978536a9c94c4e8485c8c6f1db45e52e12fa
Shawn Patrick Rice <rice@shawnrice.org>
2018-03-27 18:29:13 -0400

    saving work before switching computers

---
+++ packages/new-example/src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+const App = <div>"Hi"</div>;
+
+render(App, document.getElementById('app'));
---
+++ packages/new-example/webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+
+module.exports = {
+  entry: path.resolve(__dirname, 'src/index.js'),
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'main.js',
+    publicPath: '/',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js/,
+        use: 'babel-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 1337,
+  },
+};
