dcf2eae33d512fc9870c851340ec553d982c3de5
Shawn Patrick Rice <rice@shawnrice.org>
2018-03-29 15:42:59 -0400

    change babelrc to a js file; add in entry to ignore building test files

---
+++ .babelrc.js
@@ -0,0 +1,42 @@
+module.exports = {
+  presets: [
+    [
+      '@babel/env',
+      {
+        targets: ['IE 11', '>1%'],
+        modules: false,
+      },
+    ],
+    '@babel/stage-0',
+    '@babel/react',
+  ],
+  ignore: ['**/*.test.js'],
+  env: {
+    production: {
+      plugins: ['transform-react-remove-prop-types', 'transform-es2015-modules-commonjs'],
+      presets: [
+        [
+          '@babel/env',
+          {
+            targets: ['IE 11', '>1%'],
+            modules: false,
+          },
+        ],
+        '@babel/stage-0',
+        '@babel/react',
+      ],
+    },
+    test: {
+      presets: [
+        [
+          '@babel/env',
+          {
+            modules: 'commonjs',
+          },
+        ],
+        '@babel/stage-0',
+        '@babel/react',
+      ],
+    },
+  },
+};
