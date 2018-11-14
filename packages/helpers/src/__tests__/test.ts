03bd0f8a9d0fa94fc9022f84f0e1c9cd8aa1b03b
Shawn Patrick Rice <rice@shawnrice.org>
2018-11-14 01:06:59 -0500

    update tests; remove old tests

--- packages/helpers/src/__tests__/test.ts
+++ packages/helpers/src/__tests__/test.ts
@@ -1,47 +1,7 @@
 /* global describe, it, expect */
-import { hasErrors, runValidations, emptyArray, noop, hasOwnProperty, classes } from '..';
+import { noop, hasOwnProperty, classes } from '../index';
 
 describe('Shared helper functions tests', () => {
-  it('hasErrors should filter all `[false, false, false]` to be false', () => {
-    expect(hasErrors([false, false, false])).toBe(false);
-  });
-
-  it("hasErrors should filter all `['test', false, false]` to be true", () => {
-    expect(hasErrors(['test', false, false])).toBe(true);
-  });
-
-  it('hasErrors should have an empty array be false', () => {
-    expect(hasErrors([])).toBe(false);
-  });
-
-  it('runValidations should pass', () => {
-    const validate = value => (value === true ? false : 'should be true');
-    expect(runValidations(validate, true)).toEqual(expect.arrayContaining([false]));
-  });
-
-  it('runValidations should fail on array of functions that return messages', () => {
-    const validate = [() => 'strings indicate failure', value => (value === true ? 'error' : false)];
-    expect(runValidations(validate, true)).toEqual(expect.arrayContaining(['strings indicate failure', 'error']));
-  });
-
-  it('runValidations should fail', () => {
-    const validate = value => (value === true ? false : 'should be true');
-    expect(runValidations(validate, false)).toEqual(expect.arrayContaining(['should be true']));
-  });
-
-  it('runValidations should return false array on non-functions/arrays', () => {
-    expect(runValidations('string', false)).toEqual(emptyArray);
-  });
-
-  it('runValidations on ["string", fn => false] shoud be [false,false]', () => {
-    expect(runValidations([false, () => false], false)).toEqual(expect.arrayContaining([false, false]));
-  });
-
-  it('runValidations with fn that returns array to pass', () => {
-    const validate = value => [!value.length > 2, !value];
-    expect(runValidations(validate, 'test')).toEqual(expect.arrayContaining([false, false]));
-  });
-
   it('noop returns undefined', () => {
     expect(noop()).toBe(undefined);
   });
